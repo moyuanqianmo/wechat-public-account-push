2792cdb20fee75c85c5cbf0264a0b5a3afbd2771
moyuanqianmo <96183045+moyuanqianmo@users.noreply.github.com>
2023-04-24 23:14:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
   USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx998c6a80a97f133e',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'ecf8bf12b5bbb4f1e71ac7644593787e',
+  APP_SECRET: '',
 
   PROVINCE: '广东',
   CITY: '广州',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小可爱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '0001',
+      id: 'ojF_X5lHt4vQw5OmK2vkiwW0kbjo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
