d92bcc409bb9229c167e909c6c0e6e7f9fd1ca12
moyuanqianmo <96183045+moyuanqianmo@users.noreply.github.com>
2023-04-27 22:56:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,13 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
- 
+ USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx998c6a80a97f133e',
+ //wx998c6a80a97f133e
+  APP_ID: '',
 
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'ecf8bf12b5bbb4f1e71ac7644593787e',
+  // 使用微信测试号：公众号APP_SECRET ecf8bf12b5bbb4f1e71ac7644593787e
+  APP_SECRET: '',
 
   PROVINCE: '广东',
   CITY: '广州',
@@ -21,9 +22,10 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小可爱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ojF_X5lHt4vQw5OmK2vkiwW0kbjo',
+      id: 'ed0f6ca4ac4c4ea1925da56f883f18b3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hfPPhkiMcSoCs_Pz68ZIvyDlBX471jjrXE6HVEEyjxE',
+     //hfPPhkiMcSoCs_Pz68ZIvyDlBX471jjrXE6HVEEyjxE
+      useTemplateId: 'ed0f6ca4ac4c4ea1925da56f883f18b3',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
